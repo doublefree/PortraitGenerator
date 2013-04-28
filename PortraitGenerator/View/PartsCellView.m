ba7c77e684cbf4aaadec71cc7720401acb265421
Takuya Watabe <takuya.watabe@gmail.com>
2013-04-29 08:18:17 +0900

    parts button image is modified

--- PortraitGenerator/View/PartsCellView.m
+++ PortraitGenerator/View/PartsCellView.m
@@ -9,6 +9,7 @@
 #import "AppDelegate.h"
 #import "PartsCellView.h"
 #import "Parts.h"
+#import "cocos2d.h"
 
 @interface PartsCellView()
 @property (retain, nonatomic) IBOutlet UIButton *button;
@@ -43,8 +44,32 @@
 - (void)setData:(NSDictionary*)parts {
     self.parts = parts;
     [self.button setTitle:@"" forState:UIControlStateNormal];
-    UIImage* bgImage = [UIImage imageNamed:[parts objectForKey:PartsKeyDataPartsBaseFilePath]];
-    [self.button setBackgroundImage:bgImage forState:UIControlStateNormal];
+    
+    CCNode* node = [CCNode node];
+    
+    NSString* basePath = [parts objectForKey:PartsKeyDataPartsBaseFilePath];
+    CCSprite* sprite = [CCSprite spriteWithFile:basePath];
+    sprite.anchorPoint  = CGPointZero;
+    [node addChild:sprite];
+    NSString* framePath = [parts objectForKey:PartsKeyDataPartsFrameFilePath];
+    if ([framePath length] > 0) {
+        CCSprite* frameSprite = [CCSprite spriteWithFile:framePath];
+        frameSprite.anchorPoint  = CGPointZero;
+        [node addChild:frameSprite];
+    }
+    
+    int tx = sprite.contentSize.width;
+    int ty = sprite.contentSize.height;
+    
+    CCRenderTexture *renderer = [CCRenderTexture renderTextureWithWidth:tx height:ty];
+    
+    [renderer begin];
+    [node visit];
+    [renderer end];
+    
+    UIImage* image = [renderer getUIImage];
+    [self.button setImage:image forState:UIControlStateNormal];
+    //[self.button setBackgroundImage:image forState:UIControlStateNormal];
 }
 
 - (void)dealloc {
