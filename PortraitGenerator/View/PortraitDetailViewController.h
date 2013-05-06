353c4b5f4b61548f6456a3e740cd63c70855fc6f
Takuya Watabe <takuya.watabe@gmail.com>
2013-05-06 17:09:52 +0900

    sms

--- PortraitGenerator/View/PortraitDetailViewController.h
+++ PortraitGenerator/View/PortraitDetailViewController.h
@@ -7,7 +7,11 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <MessageUI/MessageUI.h>
+#import <MessageUI/MFMessageComposeViewController.h>
+#import <AddressBook/AddressBook.h>
+#import <AddressBookUI/AddressBookUI.h>
 
-@interface PortraitDetailViewController : UIViewController
+@interface PortraitDetailViewController : UIViewController <MFMessageComposeViewControllerDelegate, ABPeoplePickerNavigationControllerDelegate>
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil name:(NSString*)name;
 @end
--- PortraitGenerator/View/PortraitDetailViewController.m
+++ PortraitGenerator/View/PortraitDetailViewController.m
@@ -9,6 +9,9 @@
 #import "PortraitDetailViewController.h"
 #import "Portrait.h"
 
+const NSString* IMAGE_POST_URL = @"http://wiz-r.com/portrait/save.php";
+const NSString* SHOW_URL_FORMAT = @"http://wiz-r.com/portrait/show.php?id=%@";
+
 @interface PortraitDetailViewController ()
 @property (retain, nonatomic) NSString* name;
 @property (retain, nonatomic) UIImage* image;
@@ -16,6 +19,7 @@
 @property (retain, nonatomic) IBOutlet UILabel *titleTextField;
 - (IBAction)lineButtonPushed:(id)sender;
 - (IBAction)closeButtonPushed:(id)sender;
+- (IBAction)smsButtonPushed:(id)sender;
 @end
 
 @implementation PortraitDetailViewController
@@ -72,5 +76,81 @@
     [UIView setAnimationTransition:UIViewAnimationTransitionFlipFromRight forView:self.view.window.rootViewController.view cache:YES];
     [self.view removeFromSuperview];
     [UIView commitAnimations];
+}
+
+- (IBAction)smsButtonPushed:(id)sender {
+    ABPeoplePickerNavigationController *picker = [[ABPeoplePickerNavigationController alloc] init];
+    picker.peoplePickerDelegate = self;
+    [self presentViewController:picker animated:YES completion:nil];
+}
+
+- (void)messageComposeViewController:(MFMessageComposeViewController *)controller didFinishWithResult:(MessageComposeResult)result {
+    [self dismissModalViewControllerAnimated:YES];
+}
+
+- (NSString*)uploadPngFile
+{
+    NSMutableURLRequest *request = [[NSMutableURLRequest alloc] init] ;
+    [request setURL:[NSURL URLWithString:(NSString*)IMAGE_POST_URL]];
+    [request setHTTPMethod:@"POST"];
+    
+    NSMutableData *body = [NSMutableData data];
+    NSString *boundary = @"---------------------------168072824752491622650073";
+    NSString *contentType = [NSString stringWithFormat:@"multipart/form-data; boundary=%@", boundary];
+    [request addValue:contentType forHTTPHeaderField:@"Content-Type"];
+    
+    [body appendData:[[NSString stringWithFormat:@"--%@\r\n", boundary] dataUsingEncoding:NSUTF8StringEncoding]];
+    [body appendData:[@"Content-Disposition: form-data; name=\"upfile\"; filename=\"portrait.png\"\r\n" dataUsingEncoding:NSUTF8StringEncoding]];
+    [body appendData:[@"Content-Type: image/png\r\n\r\n" dataUsingEncoding:NSUTF8StringEncoding]];
+    [body appendData:[NSData dataWithData:UIImagePNGRepresentation(self.image)]];
+    [body appendData:[@"\r\n" dataUsingEncoding:NSUTF8StringEncoding]];
+    [body appendData:[[NSString stringWithFormat:@"--%@--\r\n", boundary] dataUsingEncoding:NSUTF8StringEncoding]];
+    
+    [request setHTTPBody:body];
+    
+    NSData *returnData = [NSURLConnection sendSynchronousRequest:request returningResponse:nil error:nil];
+    NSString *filename = [[NSString alloc] initWithData:returnData encoding:NSUTF8StringEncoding];
+    
+    return filename;
+}
+
+-(void)peoplePickerNavigationControllerDidCancel:(ABPeoplePickerNavigationController *)peoplePicker
+{
+    [peoplePicker dismissViewControllerAnimated:YES completion:nil];
+}
+
+-(BOOL)peoplePickerNavigationController:(ABPeoplePickerNavigationController *)peoplePicker shouldContinueAfterSelectingPerson:(ABRecordRef)person
+{
+    ABMultiValueRef phoneRef = ABRecordCopyValue(person, kABPersonPhoneProperty);    
+    if (ABMultiValueGetCount(phoneRef) > 0) {
+        NSString* phone = (NSString *)(ABMultiValueCopyValueAtIndex(phoneRef, 0));
+        
+        if(![MFMessageComposeViewController canSendText]) {
+            return NO;
+        }
+        MFMessageComposeViewController *picker = [[MFMessageComposeViewController alloc] init];
+        picker.messageComposeDelegate = self;
+        
+        NSString* imageId = [self uploadPngFile];
+        if ([imageId isEqualToString:@"NG"]) {
+            return NO;
+        }
+        NSString* body = [NSString stringWithFormat:(NSString*)SHOW_URL_FORMAT, imageId];
+        picker.body = body;
+        picker.recipients = [NSArray arrayWithObjects:phone, nil];
+        [peoplePicker dismissViewControllerAnimated:YES completion:^(void){
+            [self presentModalViewController:picker animated:YES];
+        }];
+        return NO;
+    } else {
+        [peoplePicker dismissViewControllerAnimated:YES completion:nil];
+        return NO;
+    }
+}
+
+-(BOOL)peoplePickerNavigationController:(ABPeoplePickerNavigationController *)peoplePicker shouldContinueAfterSelectingPerson:(ABRecordRef)person property:(ABPropertyID)property identifier:(ABMultiValueIdentifier)identifier
+{
+    [peoplePicker dismissViewControllerAnimated:YES completion:nil];
+    return YES;
 }
 @end
