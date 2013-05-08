1f654ad55b23c7a3da303af0553b3edeceda4843
Takuya Watabe <takuya.watabe@gmail.com>
2013-05-08 20:11:00 +0900

    save view modified

--- PortraitGenerator/View/SaveViewController.m
+++ PortraitGenerator/View/SaveViewController.m
@@ -48,8 +48,12 @@
     self.textField.placeholder = NSLocalizedString(@"save_text_placeholder", @"Please Input the Name of Portrait");
     //self.saveButton.titleLabel.text = NSLocalizedString(@"save_button_text", @"Save");
     //self.cancelButton.titleLabel.text = NSLocalizedString(@"save_cancel_button_text", @"Cancel");
-    if (self.name) {
+    if ([self.name length] > 0) {
         self.textField.text = self.name;
+    } else {
+        NSDateFormatter* refDateFormattter = [[NSDateFormatter alloc] init];
+        [refDateFormattter setDateFormat:@"yyyy-MM-dd"];
+        self.textField.text = [refDateFormattter stringFromDate:[NSDate date]];
     }
     self.textField.delegate = self;
 }
@@ -67,15 +71,58 @@
 - (IBAction)okButtonPushed:(id)sender {
     UIImageWriteToSavedPhotosAlbum(self.image, self, @selector(image:didFinishSavingWithError:contextInfo:), NULL );
     NSString* name = self.textField.text;
-    [Portrait add:self.figureSet withName:name image:self.image];
     
-    PortraitDetailViewController* detailViewController = [[PortraitDetailViewController alloc] initWithNibName:@"PortraitDetailViewController" bundle:nil name:name];
+    if ([name length] <= 0) {
+        NSString* title = NSLocalizedString(@"save_error_dialog_title", @"Error");
+        NSString* message = NSLocalizedString(@"save_error_dialog_message_input_title", @"Please input title");
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:title message:message delegate:nil cancelButtonTitle:nil otherButtonTitles:@"OK", nil];
+        [alert show];
+        return;
+    }
+    
+    self.name = name;
+    
+    NSMutableDictionary* portraitList = [Portrait list];
+    NSArray* nameList = [portraitList allKeys];
+    for(NSString* portraitName in nameList){
+        if ([name isEqualToString:portraitName]) {
+            NSString* title = NSLocalizedString(@"save_error_dialog_title", @"Error");
+            NSString* message = NSLocalizedString(@"save_dialog_message_duplicate_portrait", @"Please input title");
+            UIAlertView *alert = [[UIAlertView alloc] initWithTitle:title message:message delegate:self cancelButtonTitle:@"Cancel" otherButtonTitles:@"OK", nil];
+            [alert show];
+            return;
+        }
+    }
+    
+    [self saveAndClose];
+}
+
+- (void)saveAndClose
+{
+    [Portrait add:self.figureSet withName:self.name image:self.image];
+    
+    PortraitDetailViewController* detailViewController = [[PortraitDetailViewController alloc] initWithNibName:@"PortraitDetailViewController" bundle:nil name:self.name];
     [UIView beginAnimations:nil context:NULL];
     [UIView setAnimationDuration:0.7];
     [UIView setAnimationTransition:UIViewAnimationTransitionFlipFromRight forView:self.view.window.rootViewController.view cache:YES];
     [self.view.window.rootViewController.view addSubview:detailViewController.view];
     [UIView commitAnimations];
     [self.view removeFromSuperview];
+}
+
+-(void)alertView:(UIAlertView*)alertView clickedButtonAtIndex:(NSInteger)buttonIndex {
+    
+    switch (buttonIndex) {
+        case 0:
+            NSLog(@"Canceled");
+            break;
+        case 1:
+            [self saveAndClose];
+            NSLog(@"Saved");
+            break;
+        default:
+            break;
+    }
 }
 
 - (void)closeView
