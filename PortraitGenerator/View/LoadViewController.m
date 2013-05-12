24ea7df55f0c6fcd0f80f47af9ed60934a59f8cb
Takuya Watabe <takuya.watabe@gmail.com>
2013-05-12 10:31:38 +0900

    load view swpipe

--- PortraitGenerator/View/LoadViewController.m
+++ PortraitGenerator/View/LoadViewController.m
@@ -67,11 +67,22 @@ int const AlertTagDeletePortrait = 1;
         UIViewController* controller;
         controller = [[UIViewController alloc] initWithNibName:@"PortraitCellView" bundle:nil];
         cell = (PortraitCellView*)controller.view;
+        
         NSString* name = [nameList objectAtIndex:indexPath.row];
         cell.nameLabel.text = name;
         NSDictionary* dictionary = [portraitList objectForKey:name];
         NSData* data = [dictionary objectForKey:@"image"];
         cell.image.image = [UIImage imageWithData:data];
+        
+        UISwipeGestureRecognizer* gestureRight = [[UISwipeGestureRecognizer alloc]
+         initWithTarget:cell action:@selector(didSwipeCell:)];
+        gestureRight.direction = UISwipeGestureRecognizerDirectionRight;
+        [cell addGestureRecognizer:gestureRight];
+        
+        UISwipeGestureRecognizer* gestureLeft = [[UISwipeGestureRecognizer alloc]
+                                             initWithTarget:cell action:@selector(didSwipeCell:)];
+        gestureLeft.direction = UISwipeGestureRecognizerDirectionLeft;
+        [cell addGestureRecognizer:gestureLeft];
     }
     return cell;
 }
--- PortraitGenerator/View/PortraitCellView.m
+++ PortraitGenerator/View/PortraitCellView.m
@@ -11,7 +11,7 @@ int const CELL_HEIGHT = 64.0f;
 
 @interface PortraitCellView()
 - (IBAction)deleteButtonPushed:(id)sender;
-- (IBAction)loadButtonPushed:(id)sender;
+@property (retain, nonatomic) IBOutlet UIButton *delButton;
 @end
 
 @implementation PortraitCellView
@@ -30,24 +30,26 @@ int const CELL_HEIGHT = 64.0f;
     [super setSelected:NO animated:animated];
 }
 
+- (void)didSwipeCell:(UISwipeGestureRecognizer*)swipeRecognizer
+{
+    self.delButton.hidden = !self.delButton.hidden;
+}
+
 + (float)cellHeight {
     return CELL_HEIGHT;
 }
+
 - (void)dealloc {
     [_nameLabel release];
     [_nameLabel release];
     [_image release];
+    [_delButton release];
     [super dealloc];
 }
+
 - (IBAction)deleteButtonPushed:(id)sender {
     NSDictionary* dictionary = [NSDictionary dictionaryWithObject:self.nameLabel.text forKey:@"name"];
     NSNotification* nc = [NSNotification notificationWithName:NOTIFICATION_DELETE_BUTTON_PUSHED object:self userInfo:dictionary];
-    [[NSNotificationCenter defaultCenter] postNotification:nc];
-}
-
-- (IBAction)loadButtonPushed:(id)sender {
-    NSDictionary* dictionary = [NSDictionary dictionaryWithObject:self.nameLabel.text forKey:@"name"];
-    NSNotification* nc = [NSNotification notificationWithName:NOTIFICATION_LOAD_BUTTON_PUSHED object:self userInfo:dictionary];
     [[NSNotificationCenter defaultCenter] postNotification:nc];
 }
 @end
